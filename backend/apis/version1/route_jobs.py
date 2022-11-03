bb26a0a1f6ba6253b1211c20c2fe34139ee4815a
Andrés Court <andres@andrescourt.com>
2022-11-03 17:56:45 -0500

    Reading one job

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from db.session import get_db
 from db.models.jobs import Job
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job
+from db.repository.jobs import create_new_job, retreive_job
 
 router = APIRouter()
 
@@ -13,4 +13,15 @@ router = APIRouter()
 def create_job(job: JobCreate, db: Session = Depends(get_db)):
     current_user = 1
     job = create_new_job(job=job, db=db, owner_id=current_user)
+    return job
+
+
+@router.get("/{id}", response_model=ShowJob)
+def read_job(id: int, db: Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with this id {id} does not exist")
+
     return job
--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -14,3 +14,22 @@ def test_create_job(client):
     assert response.status_code == 200
     assert response.json()["company"] == "doogle"
     assert response.json()["description"] == "python"
+
+
+def test_read_job(client):
+    data = {
+        "title": "SDE super",
+        "company": "doogle",
+        "company_url": "www.doogle.com",
+        "location": "USA,NY",
+        "description": "python",
+        "date_posted": "2022-03-20"
+    }
+    response = client.post("/jobs/", json.dumps(data))
+
+    response = client.get("/jobs/1/")
+    assert response.status_code == 200
+    assert response.json()['title'] == "SDE super"
+
+    response = client.get("/jobs/2")
+    assert response.status_code == 404
