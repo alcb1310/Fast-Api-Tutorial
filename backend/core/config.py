a76775c6fecf7ee1e2974f79b916bc2b129a76b4
Andrés Court <andres@andrescourt.com>
2022-11-03 18:50:17 -0500

    create access tokens

--- backend/core/config.py
+++ backend/core/config.py
@@ -19,5 +19,10 @@ class Settings:
     POSTGRES_DB: str = os.getenv('POSTGRES_DB', 'tdd')
     DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
+    # os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 2
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    ALGORITHM: str = os.getenv('ALGORITHM')
+
 
 settings = Settings()
